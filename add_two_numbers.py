431a78c385da28ca4e2189bbb82958cf2dc635fe
Johan Rex <52hands@gmail.com>
2021-12-01 15:45:35 +0100

    initial commit

---
+++ add_two_numbers.py
@@ -0,0 +1,83 @@
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# The number of nodes in each linked list is in the range [1, 100].
+# 0 <= Node.val <= 9
+# It is guaranteed that the list represents a number that does not have leading zeros.
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        result_node_first = None        
+        result_node_prev = None
+
+        carry = 0
+        while True:
+            if l1 is None and l2 is None and carry == 0:
+                break
+
+            l1val = l1.val if l1 is not None else 0
+            l2val = l2.val if l2 is not None else 0
+
+            total_sum = carry + l1val + l2val
+
+            current_node_sum = total_sum % 10
+            carry = total_sum // 10
+
+            result_node_current = ListNode(current_node_sum)
+
+            if result_node_prev == None:
+                result_node_first = result_node_current
+            else:
+                result_node_prev.next = result_node_current
+
+            result_node_prev = result_node_current
+
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+
+        return result_node_first
+
+
+# l1n1 = ListNode(2, next=ListNode(4, next=ListNode(3, next=ListNode(9))))
+# l2n1 = ListNode(5, next=ListNode(6, next=ListNode(4)))
+
+def create_linked_list(arr):
+    first_node = None
+    prev_node = None
+    for val in arr:
+        current_node = ListNode(val)
+
+        if prev_node is not None:
+            prev_node.next = current_node
+
+        prev_node = current_node
+        if first_node is None:
+            first_node = current_node
+
+    return first_node
+
+def print_linked_list(l):
+    arr = []
+    while True:
+        arr.append(l.val)
+        l = l.next
+        if l is None:
+            break
+    print(arr)
+
+l1 = create_linked_list([9,9,9,9,9,9,9])
+l2 = create_linked_list([9,9,9,9])
+
+s = Solution()
+l = s.addTwoNumbers(l1, l2)
+print_linked_list(l)
+
---
+++ two sum.py
@@ -0,0 +1,88 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+ 
+
+Constraints:
+
+    2 <= nums.length <= 104
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+    Only one valid answer exists.
+
+ 
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+"""
+
+from typing import List
+
+def sort_func(tup):
+    return tup[0]
+
+class Solution:
+
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        indexes = list(range(len(nums)))
+
+        zipped = zip(nums, indexes)
+
+        sorted_tuples = sorted(zipped, key=sort_func)
+
+        idx_low = 0
+        idx_high = len(nums)-1
+
+        #assume solution exists
+        while True:
+            
+            val_low = sorted_tuples[idx_low][0]
+            val_high = sorted_tuples[idx_high][0]
+
+            sum = val_low + val_high
+
+            if sum == target:
+                break
+            elif sum < target:
+                idx_low += 1
+            else:
+                idx_high -= 1
+
+        return [sorted_tuples[idx_low][1], sorted_tuples[idx_high][1]]
+
+
+
+import random
+nums = random.sample(range(-109,109), 104)
+target = nums[50] + nums[51]
+
+print(f'target: {target}')
+
+s = Solution()
+ans = s.twoSum(nums, target)
+print(ans)
+
+print(f'gives {nums[ans[0]]+nums[ans[1]]}')
